1f7e99de92b99857cf858eb8a83100bc0b219dec
vtahmi <vtahmis@gmail.com>
2026-04-16 10:39:36 +0100

    Update

--- books_api/serializers.py
+++ books_api/serializers.py
@@ -8,7 +8,21 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 class BookSerializer(serializers.ModelSerializer):
-    authors = AuthorSerializer(many=True, read_only=True)
+    authors = AuthorSerializer(many=True)
     class Meta:
         model = Book
         fields = ['id', 'title', 'pages', 'description', 'authors']
+
+    def create(self, validated_data):
+        authors_data = validated_data.pop('authors')
+        book = Book.objects.create(**validated_data)
+        authors = [author['name'] for author in authors_data]
+        existing_authors = Author.objects.filter(name__in=authors)
+        new_authors_names = set(authors) - set(existing_authors.values_list('name', flat=True))
+        new_authors = [Author(name=name) for name in new_authors_names]
+        Author.objects.bulk_create(new_authors)
+        all_authors = list(existing_authors) + new_authors
+        book.authors.set(all_authors)
+        return book
+
+ 
